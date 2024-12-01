ee3480150566073ae5e1d1d71eb8b16d309d43bd
Matheus.Santossss <matheuzsg1@gmail.com>
2024-12-01 18:05:59 -0300

    Integração com Front

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes';
 import authRoutes from './routes/authRoutes';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 
 app.use(userRoutes, authRoutes);
